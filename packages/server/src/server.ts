30c7e0402cec443a8e13760056f28ef32171be65
Ben Rabi <benr@wix.com>
2023-07-28 18:18:33 +0300

    add(): init commit

---
+++ packages/server/src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { createContext } from './context';
+import { appRouter } from './router';
+
+const PORT = 3001;
+const app = express();
+
+app.use(cors());
+
+app.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
+
+app.listen(PORT, () => console.log(`🚀 Server ready at http://localhost:${PORT}`));
